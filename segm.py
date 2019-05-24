6e54df720428e3e984508edf4af5b65548bcc957
Eduardo Zimermam <eduardozimerman@live.com>
2019-05-24 16:57:17 -0300

    Término da segmentação das sementes

--- segm.py
+++ segm.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+from extract import getConvexHu
 
 def segmentacao(img):
 	
-	grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	red, green, blue = cv2.split(img)
 
-	_, thresh = cv2.threshold(grayImg, 100, 255, cv2.THRESH_OTSU)
+	hue, saturation, value = cv2.split(hsvImg)
+
+
+
+	blur = cv2.GaussianBlur(saturation, (17,17), 0)
+
+
+	_, thresh = cv2.threshold(blur, 200, 255, cv2.THRESH_BINARY_INV)
+
 
 	cv2.imshow('Imagem Binarizada', thresh)
 	cv2.waitKey(0)
-
-
