30061ec82f0e21e6b662c1ee7bc0d57baa32a28a
louisekn <lk16@inf.ufpr.br>
2019-06-08 22:19:33 -0300

    segmentacao sem buracos

--- segm.py
+++ segm.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import cv2
+import numpy as np
 
 def segmentacao(img):
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	
-	imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-	red, green, blue = cv2.split(img)
+	imgGray = cv2.cvtColor(hsvImg, cv2.COLOR_BGR2GRAY)
 
-	hue, saturation, value = cv2.split(hsvImg)
+	kernel = np.ones((11,11),np.uint8)
 
-	blur = cv2.GaussianBlur(saturation, (25,25), 0)
+	blur = cv2.GaussianBlur(imgGray, (11,11), 0)
 
-	_, thresh = cv2.threshold(blur, 175, 255, cv2.THRESH_BINARY_INV)
+	thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+            cv2.THRESH_BINARY,11,2)
+	_, thresh = cv2.threshold(blur, 163, 255, cv2.THRESH_BINARY_INV)
+	thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+	thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+	
 
-	thresh = thresh * imgGray
-
-	# cv2.imshow('Imagem Binarizada', blue)
+	thresh = thresh * gray
+	# cv2.imshow("Imagem", thresh)
 	# cv2.waitKey(0)
 
 	return(thresh)
